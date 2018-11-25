989c19df7697ed5a94fa3c2d2ac8134560c4d7ff
Nikos Koukis <nikolaos@slamcore.com>
2018-11-25 02:32:50 +0000

    Add summary to index.wiki file, have file with all imported notes

--- convert-notes.py
+++ convert-notes.py
@@ -1,25 +1,77 @@
 #!/usr/bin/env python3
 
-""" Scirpt fro converting a vim-notes to vimwiki structure"""
-import click
+"""
+Scirpt for converting a vim-notes directory to the corresponding vimwiki
+structure
+"""
+
+import datetime
 import os
-import sys
 import re
+import sys
+
+import click
 
 
 @click.command()
-@click.option('--notes-dir', required=True, help='Path to notes directory')
-@click.option('--vw-dir', required=True, help='Path to notes directory')
-def main(notes_dir: str, vw_dir: str):
+@click.option('--notes-dir', required=True, help='Path to vim-notes directory')
+@click.option('--vw-dir', required=True, help='Path to vimwiki directory')
+@click.option('--interactive', is_flag=True,
+              help='Prompt in case overwrite is needed')
+def main(notes_dir: str, vw_dir: str, interactive):
     """Main."""
 
-    # call  covnert_file for all files
-    for fname in [f for f in os.listdir(notes_dir) if f.endswith(".md")]:
-        print("Converting: {}".format(fname))
+    # sanitize data
+    for d in notes_dir, vw_dir:
+        if not os.path.isdir(d):
+            print("\"{}\" is not a directory. Exiting...".format(d))
+            sys.exit(1)
+
+    # call  convert_file for all files
+    print("Converting notes...")
+    files_list = [f for f in os.listdir(notes_dir) if f.endswith(".md")]
+    for i, fname in enumerate(files_list):
+        print("{}. Converting: {}".format(i, fname))
         fname_wo_ext = fname.rsplit('.', maxsplit=1)[0]
         fname_wiki = os.path.join(vw_dir, "{}.wiki".format(fname_wo_ext))
         fname_full = os.path.join(notes_dir, fname)
+
+        if os.path.exists(fname_wiki):
+            if interactive:
+                ans = input("\"{}\" already exists. Overwrite? [Y/n]"
+                            .format(fname_wiki))
+                if ans not in ['Y', 'y', '']:
+                    print("Skipping file...")
+                    continue
+            else:
+                print("[!] \"{}\" already exists, overwriting...".format(fname_wiki))
+
         convert_file(fname_full, fname_wiki)
+
+    # Modify index.wiki
+    print("Adding link to notes in index.wiki...")
+    index_file = os.path.join(vw_dir, "index.wiki")
+    if not os.path.isfile(index_file):
+        print("Cannot detect {} file. Exiting...".format(index_file))
+        sys.exit(2)
+
+    # Summarise imported notes file in another `.wiki` page
+    notes_wiki_fname = "vim_notes_imported"
+    notes_wiki_fpath = os.path.join(vw_dir,
+                                    ".".join([notes_wiki_fname, 'wiki']))
+    with open(notes_wiki_fpath, 'w') as notes_f:
+        notes_f.write("= Imported notes (former vim-notes) =\n\n")
+        notes_f.write("Notes imported on {}\n"
+                      .format(datetime.datetime.now()
+                              .strftime("%Y/%m/%d - %I:%M%p")))
+
+        for fname in files_list:
+            fname_wo_ext = fname.rsplit('.', maxsplit=1)[0]
+            notes_f.write("\t* [[{}]]\n".format(fname_wo_ext))
+
+    with open(index_file, 'a') as index_f:
+        index_f.write("* [[{}|Imported notes from vim-notes directory]]\n\n"
+                      .format(notes_wiki_fname))
 
 
 def convert_file(src: str, dst: str):
@@ -33,8 +85,6 @@ def convert_file(src: str, dst: str):
     # Process
     ###########################################################################
     # write note title
-    print("lines: ", lines)
-    print(lines[0])
     lines[0] = "= {} =\n".format(lines[0].rstrip())
 
     lines = sed_headers(lines)
@@ -52,7 +102,8 @@ def sed_headers(lines: list) -> str:
     """Convert Markdown '#' headers to Pandoc = * = headers."""
 
     for i, line in enumerate(lines):
-        m = re.match('(#+) (.*)$', line)
+        # TODO - test this
+        m = re.match('^(#+) (.*)$', line)
         if m:
             lvl = len(m.group(1))
             s = m.group(2)
@@ -65,12 +116,13 @@ def sed_tags(lines: list) -> str:
     """Convert vim-notes '@' tags to vimwiki :*: tags."""
 
     for i, line in enumerate(lines):
-        m = re.match('@(\w+)$', line)
+        m = re.match('^@([a-zA-Z-]+)$', line)
         if m:
             s = m.group(1)
             lines[i] = ":{0}:\n".format(s)
 
     return lines
+
 
 def sed_bullets(lines: list) -> str:
     """Convert vim-notes '•', '◦', '▸' tags to vimwiki :*: tags."""
@@ -79,6 +131,7 @@ def sed_bullets(lines: list) -> str:
         '•': '*',
         '◦': '*',
         '▸': '*',
+        '▹': '*',
     }
 
     # Line must start with these characters (excpet spaces)
@@ -108,6 +161,3 @@ def sed_snippets(lines: list) -> str:
 
 if __name__ == '__main__':
     main()
-
-
-
